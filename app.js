aee78e11d54e1f89d16436341637fd867c33b48f
Mcclayn798 <mnkmcc9@gmail.com>
2023-06-15 05:20:11 +0300

    son

---
+++ app.js
@@ -0,0 +1,22 @@
+// // let b =5;
+
+// // function Method(){
+// //     let  c = 14;
+// // }
+// // console.log(b);
+
+// // Method()
+
+// let a = {
+//     name : "kadir",
+//     soyisim : "gürsoy",
+//     yas : 40
+// }
+// // console.log(typeof a)
+
+
+// let func = function(){
+//     console.log("Merhaba");
+// }
+
+// func()
